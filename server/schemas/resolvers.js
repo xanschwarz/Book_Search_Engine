1b76cad7c491fa7c576ad5a85cfa0b644b01a3fe
Xander <xanschwarz@gmail.com>
2021-11-04 15:35:08 -0700

    Resolvers partially written.

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,51 @@
-const {} = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
-  //
+  Query: {
+    // Returns a User type.
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id });
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+  },
+  Mutation: {
+    // Accepts an email and password as parameters; returns an Auth type.
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('No user found with this email address');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    // Accepts a username, email, and password as parameters; returns an Auth type.
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+    // Accepts a book author's array, description, title, bookId, image, and link as parameters; returns a User type. (Look into creating what's known as an input type to handle all of these parameters!)
+    saveBook: {
+      //
+    },
+    // Accepts a book's bookId as a parameter; returns a User type.
+    removeBook: {
+      //
+    },
+  },
 };
 
 module.exports = resolvers;
