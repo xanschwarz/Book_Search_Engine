ed378619e585a7cbd1fd2f8a553c9b31b710bbad
Xander <xanschwarz@gmail.com>
2021-11-04 22:40:35 -0700

    Setup for MongoDB Atlas and Heroku.

--- server/server.js
+++ server/server.js
@@ -24,6 +24,13 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/bookSearch', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
