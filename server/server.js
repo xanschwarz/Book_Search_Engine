f0e90a09f43378b77ebe9be7f7fdcfb3e9ab4083
Xander <xanschwarz@gmail.com>
2021-11-04 22:26:23 -0700

    Initial Heroku commit.

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const path = require('path');
 const db = require('./config/connection');
-// const routes = require('./routes');
 const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
