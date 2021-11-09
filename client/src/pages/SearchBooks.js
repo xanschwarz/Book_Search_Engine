69597109157ef858128900cf00e18cf810e6c94e
Xander <xanschwarz@gmail.com>
2021-11-08 16:08:04 -0800

    Removed window reload from function handleSaveBook in SearchBooks.js. This removal allows the search results to stay displayed, instead of reloading the landing page with no search results.

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -87,7 +87,6 @@ const SearchBooks = () => {
 
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
-      window.location.reload();
     } catch (err) {
       console.error(err);
     }
